9a8e661bc1a6f4259288ff9a98c6ab997eea0020
Abu <abutahir_11@yahoo.com>
2020-05-31 16:26:12 +0530

    Recursive traversals for binaryTree

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,9 +1,36 @@
 package core;
 
+import trees.RecursiveTraversal;
+import trees.TreeNode;
 
 public class Main {
 
     public static void main(String[] args){
+        TreeNode rootNode = new TreeNode(1);
+        TreeNode firstLeft = new TreeNode(2);
+        TreeNode firstRight = new TreeNode(3);
+        rootNode.left(firstLeft);
+        rootNode.right(firstRight);
+
+        TreeNode secondLeft = new TreeNode(4);
+        TreeNode secondRight = new TreeNode(5);
+
+        firstLeft.left(secondLeft);
+        firstLeft.right(secondRight);
+
+        RecursiveTraversal traversal = new RecursiveTraversal();
+
+        System.out.println("[*] Inorder Traversal ...");
+
+        traversal.inOrderTraversal(rootNode);
+
+        System.out.println("[*] PreOrder Traversal ...");
+
+        traversal.preOrderTraversal(rootNode);
+
+        System.out.println("[*] PostOrder Traversal ...");
+        traversal.postOrderTraversal(rootNode);
+
 
     }
 }
--- src/trees/RecursiveTraversal.java
+++ src/trees/RecursiveTraversal.java
@@ -3,24 +3,48 @@ package trees;
 public class RecursiveTraversal {
     public void inOrderTraversal(TreeNode root){
         if (root != null) {
-            System.out.println(root.get());
+
             TreeNode left = root.getLeft();
             TreeNode right = root.getRight();
-            if (left != null) {
+            if (left != null)
                 inOrderTraversal(left);
-            }
-            if (right != null) {
+
+            System.out.println(root.get());
+
+            if (right != null)
                 inOrderTraversal(right);
-            }
 
         }
 
     }
     public void preOrderTraversal(TreeNode root){
+        if (root != null) {
+            System.out.println(root.get());
 
+            TreeNode left = root.getLeft();
+            TreeNode right = root.getRight();
+            if (left != null)
+                preOrderTraversal(left);
+
+            if (right != null)
+                preOrderTraversal(right);
+
+        }
     }
 
     public void postOrderTraversal(TreeNode root){
+        if (root != null) {
 
+
+            TreeNode left = root.getLeft();
+            TreeNode right = root.getRight();
+            if (left != null)
+                postOrderTraversal(left);
+
+            if (right != null)
+                postOrderTraversal(right);
+
+            System.out.println(root.get());
+        }
     }
 }
